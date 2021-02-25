8115b33c59f2d62f792ef8ab6de3c6be18c91d9e
imran-synectiks <imran.mohd@synectiks.com>
2021-02-25 17:39:39 +0530

    added image part

--- src/templates/service-post.js
+++ src/templates/service-post.js
@@ -4,7 +4,9 @@ import { kebabCase } from 'lodash'
 import { Helmet } from 'react-helmet'
 import { graphql, Link } from 'gatsby'
 import Layout from '../components/Layout'
+import Testimonials from '../components/Testimonials'
 import Content, { HTMLContent } from '../components/Content'
+import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
 export const ServicePostTemplate = ({
   content,
@@ -12,7 +14,11 @@ export const ServicePostTemplate = ({
   description,
   tags,
   title,
+  testimonials,
   page1,
+  page2,
+  page3,
+  page4,
   helmet,
 }) => {
   const PostContent = contentComponent || Content
@@ -27,6 +33,41 @@ export const ServicePostTemplate = ({
               {title}
             </h1>
             <p>{description}</p>
+            <Testimonials testimonials={testimonials} />
+            <div>
+              {page1.heading}
+              <br/> 
+              {page1.description}
+              <br/>
+              <div className="tile">
+                    <div className="tile is-parent is-vertical">
+                      <article className="tile is-child">
+                        <PreviewCompatibleImage imageInfo={page1.image} />
+                      </article>
+                    </div>
+                    </div>
+            </div>
+            <div>
+              {page2.heading}
+              <br/> 
+              {page2.description}
+              <br/>
+             
+            </div>
+            <div>
+              {page3.heading}
+              <br/> 
+              {page3.description}
+              <br/>
+              
+            </div>
+            <div>
+              {page4.heading}
+              <br/> 
+              {page4.description}
+              <br/>
+               
+            </div>
             <PostContent content={content} />
             {tags && tags.length ? (
               <div style={{ marginTop: `4rem` }}>
@@ -51,8 +92,27 @@ ServicePostTemplate.propTypes = {
   content: PropTypes.node.isRequired,
   contentComponent: PropTypes.func,
   description: PropTypes.string,
+  page1: PropTypes.string,
+  testimonials: PropTypes.array,
   title: PropTypes.string,
   helmet: PropTypes.object,
+  page1: PropTypes.shape({
+    heading: PropTypes.string,
+    description: PropTypes.string,
+    image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  }),
+  page2: PropTypes.shape({
+    heading: PropTypes.string,
+    description: PropTypes.string,
+  }),
+  page3: PropTypes.shape({
+    heading: PropTypes.string,
+    description: PropTypes.string,
+  }),
+  page4: PropTypes.shape({
+    heading: PropTypes.string,
+    description: PropTypes.string, 
+  }),
 }
 
 const ServicePost = ({ data }) => {
@@ -64,6 +124,11 @@ const ServicePost = ({ data }) => {
         content={post.html}
         contentComponent={HTMLContent}
         description={post.frontmatter.description}
+        testimonials={post.frontmatter.testimonials}
+        page1={post.frontmatter.page1}
+        page2={post.frontmatter.page2}
+        page3={post.frontmatter.page3}
+        page4={post.frontmatter.page4}
         helmet={
           <Helmet titleTemplate="%s | Blog">
             <title>{`${post.frontmatter.title}`}</title>
@@ -98,6 +163,36 @@ export const pageQuery = graphql`
         title
         description
         tags
+        testimonials {
+          author
+          quote
+        }
+        page1 {
+          description
+          heading
+          image {
+            childImageSharp {
+              fluid(maxWidth: 240, quality: 64) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        page2 {
+          description
+          heading
+          
+        }
+        page3 {
+          description
+          heading
+          
+        }
+        page4 {
+          description
+          heading
+          
+        }
       }
     }
   }
