8153f831c1bf7965eda0b813124b67f1a3489779
mustafa-synectiks <35029729+mustafa-synectiks@users.noreply.github.com>
2021-03-11 15:37:57 +0530

    Wokflow added

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,21 +1,22 @@
-import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
+import CMS from 'netlify-cms-app';
+import uploadcare from 'netlify-cms-media-library-uploadcare';
+import cloudinary from 'netlify-cms-media-library-cloudinary';
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ServicePostPreview from './preview-templates/ServicePostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
-import SliderPagePreview from './preview-templates/SliderPagePreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
+import AboutPagePreview from './preview-templates/AboutPagePreview';
+import BlogPostPreview from './preview-templates/BlogPostPreview';
+import ServicePostPreview from './preview-templates/ServicePostPreview';
+import ProductPagePreview from './preview-templates/ProductPagePreview';
+import SliderPagePreview from './preview-templates/SliderPagePreview';
+import IndexPagePreview from './preview-templates/IndexPagePreview';
+import WorkflowPreview from './preview-templates/WorkflowPreview';
 
-CMS.registerMediaLibrary(uploadcare)
-CMS.registerMediaLibrary(cloudinary)
+CMS.registerMediaLibrary(uploadcare);
+CMS.registerMediaLibrary(cloudinary);
 
-CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
-CMS.registerPreviewTemplate('service', ServicePostPreview)
-CMS.registerPreviewTemplate('slider', SliderPagePreview)
-
+CMS.registerPreviewTemplate('index', IndexPagePreview);
+CMS.registerPreviewTemplate('about', AboutPagePreview);
+CMS.registerPreviewTemplate('products', ProductPagePreview);
+CMS.registerPreviewTemplate('blog', BlogPostPreview);
+CMS.registerPreviewTemplate('service', ServicePostPreview);
+CMS.registerPreviewTemplate('workflowpost', WorkflowPreview);
+CMS.registerPreviewTemplate('slider', SliderPagePreview);
